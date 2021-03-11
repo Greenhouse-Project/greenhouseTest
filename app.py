0bcb7245836c950023f11f440c867e7d7063331b
joeroybal <joewroybal@gmail.com>
2021-03-10 21:08:34 -0600

    tinkered with the app.py to get flask to properly work

--- app.py
+++ app.py
@@ -1,34 +1,34 @@
 import os
 
-from flask import render_template
+from flask import Flask,render_template
 
-from . import home
+app = Flask(__name__)
 
 # Home route
-@home.route('/')
+@app.route('/')
 def Home():
     return render_template('index.html', title='Home')
     
 # Front Room route
-@home.route('/front-room')
+@app.route('/front-room')
 def FrontRoom():
     return 'The Front Room Page works'
     # return render_template('TBD', title='Front Room')
     
 # Back Room route
-@home.route('/back-room')
+@app.route('/back-room')
 def BackRoom():
     return 'The Back Room Page works'
     # return render_template('TBD', title='Back Room')
     
 # Outside route
-@home.route('/outside')
+@app.route('/outside')
 def Outside():
     return 'The Outside Room Page works'
     # return render_template('TBD', title='Outside')
     
 # About route
-@home.route('/about')
+@app.route('/about')
 def About():
     return 'The About page is working'
 #     return render_template('TBD', title='About')
