28e9edac5b8f66f217df6bf8101d23e2162bf2ec
joeroybal <joewroybal@gmail.com>
2021-03-10 21:40:03 -0600

    tweaking

--- app/main.py
+++ app/main.py
@@ -1,16 +1,13 @@
-# Temporarily out of commision while testing
+# Temporarily out of commission while testing
+from flask import Flask#,render_template
 
+app = Flask(__name__)
 
-# import os
-
-# from flask import Flask,render_template
-
-# app = Flask(__name__)
-
-# # Home route
-# @app.route('/')
-# def Home():
-#     return render_template('index.html', title='Home')
+# Home route
+@app.route('/')
+def Home():
+    return "<h1>It works</h1>"
+    # return render_template('index.html', title='Home')
     
 # # Front Room route
 # @app.route('/front-room')
