6e25ddc7eee5bd34ec8ed1d60b5e2c775612a1f2
joeroybal <joewroybal@gmail.com>
2021-03-11 12:50:46 -0600

    aded the rest of the routes

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ app = Flask(__name__)
 def Home():
     return render_template('index.html', title='Home')
     
-# # Front Room route
-# @app.route('/front-room')
-# def FrontRoom():
-#     return 'The Front Room Page works'
-#     # return render_template('TBD', title='Front Room')
+# Front Room route
+@app.route('/front-room')
+def FrontRoom():
+    return 'The Front Room Page works'
+    # return render_template('TBD', title='Front Room')
     
-# # Back Room route
-# @app.route('/back-room')
-# def BackRoom():
-#     return 'The Back Room Page works'
-#     # return render_template('TBD', title='Back Room')
+# Back Room route
+@app.route('/back-room')
+def BackRoom():
+    return 'The Back Room Page works'
+    # return render_template('TBD', title='Back Room')
     
-# # Outside route
-# @app.route('/outside')
-# def Outside():
-#     return 'The Outside Room Page works'
-#     # return render_template('TBD', title='Outside')
+# Outside route
+@app.route('/outside')
+def Outside():
+    return 'The Outside Room Page works'
+    # return render_template('TBD', title='Outside')
     
-# # About route
-# @app.route('/about')
-# def About():
-#     return 'The About page is working'
-# #     return render_template('TBD', title='About')
+# About route
+@app.route('/about')
+def About():
+    return 'The About page is working'
+#     return render_template('TBD', title='About')
