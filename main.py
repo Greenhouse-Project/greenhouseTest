31a98594bd2b39a1998d8b123f316c51773b4b8f
joeroybal <joewroybal@gmail.com>
2021-03-13 14:52:14 -0600

    set up Database

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import os  
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hmcjtyhcsvdyvp:ae7e4a3535fa02d0a51c50d52357d7434b0ce16dd3d8caee2887adbce643d35c@ec2-54-159-175-113.compute-1.amazonaws.com:5432/d5s3splm4psiqk'
 
 # Home route
 @app.route('/')
 def Home():
     return render_template('index.html', title='Home')
     
-# Front Room route
-@app.route('/front-room')
-def FrontRoom():
-    return 'The Front Room Page works'
-    # return render_template('TBD', title='Front Room')
+# # Front Room route
+# @app.route('/front-room')
+# def FrontRoom():
+#     return 'The Front Room Page works'
+#     # return render_template('TBD', title='Front Room')
     
-# Back Room route
-@app.route('/back-room')
-def BackRoom():
-    return 'The Back Room Page works'
-    # return render_template('TBD', title='Back Room')
+# # Back Room route
+# @app.route('/back-room')
+# def BackRoom():
+#     return 'The Back Room Page works'
+#     # return render_template('TBD', title='Back Room')
     
 # Outside route
 @app.route('/outside')
@@ -35,4 +38,4 @@ def About():
 # Testing page for forms
 @app.route('/testing')
 def Testing():
-    return render_template('testing.html', title='Testing')
+    return render_template('testing.html', title='Testing') 
--- readme.md
+++ readme.md
@@ -2,12 +2,24 @@
 
 ## Locally
 
+For Linux:
+
+_for running on a server:_
+_in wsgi.py change `app.run()` to `app.run(host='0.0.0.0')`_
+_also change step 5 to `flask run --host 0.0.0.0`_
+
 1. `pip install -r requirements.txt `
 2. `source env/bin/activate`
 3. `export FLASK_ENV=development`
 4. `export FLASK_APP=main.py`
 5. `flask run`
 6. Open your browser and check `127.0.0.1:5000`
+
+For Windows:
+
+1. `pip install -r requirements.txt `
+2. `python wsgi.py`
+3. Open your browser and check `127.0.0.1:5000`
 
 ## On Heroku
 
