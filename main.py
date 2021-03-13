d4df7b32c93c892fee73eb0816055c24276487d5
joeroybal <joewroybal@gmail.com>
2021-03-13 16:47:18 -0600

    added fields for form

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import os  
-from flask import Flask,render_template,request
+from flask import Flask, config,render_template,request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, Form,validators, TextField, TextAreaField, SubmitField
 
 app = Flask(__name__)
 
+app.secret_key='sfhjdhfjklhskjfhsd'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hmcjtyhcsvdyvp:ae7e4a3535fa02d0a51c50d52357d7434b0ce16dd3d8caee2887adbce643d35c@ec2-54-159-175-113.compute-1.amazonaws.com:5432/d5s3splm4psiqk'
 
 db = SQLAlchemy(app)
 
+class ReusableForm(Form):
+    owner = TextField('Owner: ', validators=[validators.required()])
+    plant_species = TextField('Plant Species: ', validators=[validators.required()])
+    date_planted = TextField('Date Planted: ', validators=[validators.required()])
+    date_finish = TextField('Date Finished: ', validators=[validators.required()])
+    last_watered = TextField('Last Watered: ', validators=[validators.required()])
+    owner = TextField('Date PLanted: ', validators=[validators.required()])
+    temp = TextField('Temperature: ', validators=[validators.required()])
+    humidity = TextField('Humidity: ', validators=[validators.required()])
+    soil_moisture = TextField('Soil Moisture: ', validators=[validators.required()])
+    bed = TextField('Bed Number: ', validators=[validators.required()])
+    
 class Plants(db.Model):
     __tablename__ = "greenhouse"
     _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     plant_species = db.Column(db.String(100), nullable=False)
     owner = db.Column(db.String(100), nullable=False)
+    date_planted = db.Column(db.String(100), nullable=False)
     date_finish = db.Column(db.String(100), nullable=False)
     last_watered = db.Column(db.String(100), nullable=False)
     temp = db.Column(db.String(100), nullable=False)
@@ -20,9 +36,10 @@ class Plants(db.Model):
     soil_moisture = db.Column(db.String(100), nullable=False)
     bed = db.Column(db.String(100), nullable=False)
 
-    def __init__(self, plant_species, owner, date_finish, last_watered, temp, humidity, soil_moisture, bed):
+    def __init__(self, plant_species, owner,date_planted, date_finish, last_watered, temp, humidity, soil_moisture, bed):
         self.plant_species = plant_species
         self.owner = owner
+        self.date_planted = date_planted
         self.date_finish = date_finish
         self.last_watered = last_watered
         self.temp = temp
@@ -35,19 +52,7 @@ class Plants(db.Model):
 @app.route('/')
 def Home():
     return render_template('index.html', title='Home')
-    
-# # Front Room route
-# @app.route('/front-room')
-# def FrontRoom():
-#     return 'The Front Room Page works'
-#     # return render_template('TBD', title='Front Room')
-    
-# # Back Room route
-# @app.route('/back-room')
-# def BackRoom():
-#     return 'The Back Room Page works'
-#     # return render_template('TBD', title='Back Room')
-    
+
 # Outside route
 @app.route('/outside')
 def Outside():
@@ -63,20 +68,26 @@ def About():
 # Testing page for forms
 @app.route('/testing', methods = ["GET", "POST"])
 def Testing():
+    form = ReusableForm(request.form)
     if request.method == 'POST': # submit
         data= request.form # data from the form
-        plant_species = data["plant_species"]
-        owner = data["owner"]
-        date_finish = data["date_finish"]
-        last_watered = data["last_watered"]
-        temp = data["temp"]
-        humidity = data["humidity"]
-        soil_moisture = data["soil_moisture"]
-        bed = data["bed"]
-        new_data = Plants(plant_species, owner, date_finish, last_watered, temp, humidity, soil_moisture, bed)
+        date_planted = request.form['date_planted']
+        plant_species = request.form['plant_species']
+        owner = request.form['owner']
+        date_finish = request.form['date_finish']
+        last_watered = request.form['last_watered']
+        temp = request.form['temp']
+        humidity = request.form['humidity']
+        soil_moisture = request.form['soil_moisture']
+        bed = request.form['bed']
+        new_data = Plants(plant_species, owner,date_planted, date_finish, last_watered, temp, humidity, soil_moisture, bed)
         db.session.add(new_data)
         db.session.commit()
         
         user_data = Plants.query.all()
         return render_template('testing.html', user_data = user_data)
-    return render_template('testing.html', title='Testing') 
+    if form.validate():
+        flash('Hello ' + form)
+    else:
+        flash('All form fields are required')
+    return render_template('testing.html',form = form, title='Testing') 
