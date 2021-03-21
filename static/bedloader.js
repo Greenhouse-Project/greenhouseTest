ea8802a8ab01530be14256688d69ee70ddffa651
joeroybal <joewroybal@gmail.com>
2021-03-20 21:47:02 -0500

    configuering database types

--- static/bedloader.js
+++ static/bedloader.js
@@ -1,7 +1,7 @@
 function planted() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const bedNum = urlParams.get('bed');
+    const bedNum = new Number(urlParams.get('bed'));
     document.main.bedPlot.value = bedNum;
     var name = document.main.owner.value;
     var plant = document.main.plant_species.value;
