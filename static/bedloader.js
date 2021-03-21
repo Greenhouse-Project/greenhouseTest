cd9caf6fec12d94ae9026024e69681b692f08620
joeroybal <joewroybal@gmail.com>
2021-03-20 23:20:41 -0500

    went beck to testing file

--- static/bedloader.js
+++ static/bedloader.js
@@ -10,3 +10,13 @@ function planted() {
     var bedNum = document.main.bedPlot.value;
     alert("Thanks "+name+" for planting "+plant+" in bed "+bedNum);
 }
+// For form.html
+// function planted() {
+//     const queryString = window.location.search;
+//     const urlParams = new URLSearchParams(queryString);
+//     const bedNum = new Number(urlParams.get('bed'));
+//     document.main.bedPlot.value = bedNum;
+//     var name = document.main.owner.value;
+//     var plant = document.main.plant_species.value;
+//     alert("Thanks "+name+" for planting "+plant+" in bed "+bedNum);
+// }
