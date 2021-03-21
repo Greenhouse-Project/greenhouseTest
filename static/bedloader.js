255b32ea9d258acee4d8408aac39e1af737325c1
joeroybal <joewroybal@gmail.com>
2021-03-20 23:55:22 -0500

    missed typo in bedloader.js, now it is actually calling the script

--- static/bedloader.js
+++ static/bedloader.js
@@ -2,7 +2,7 @@ function planted() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const bedNum = new Number(urlParams.get('bed'));
-    document.main.bedPlot.value = bedNum;
+    document.main.bed.value = bedNum;
     var name = document.main.owner.value;
     var plant = document.main.plant_species.value;
     alert("Thanks "+name+" for planting "+plant+" in bed "+bedNum);
