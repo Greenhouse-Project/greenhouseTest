04335ec0a9309c16ee34ba630e1ce4e96790cde7
joeroybal <joewroybal@gmail.com>
2021-03-20 23:05:18 -0500

    added an "airlock" to hopfully solve the problem

--- static/bedloader.js
+++ static/bedloader.js
@@ -1,9 +1,12 @@
-function planted() {
+function populate() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const bedNum = new Number(urlParams.get('bed'));
     document.main.bedPlot.value = bedNum;
+}
+function planted() {
     var name = document.main.owner.value;
     var plant = document.main.plant_species.value;
+    var bedNum = document.main.bedPlot.value;
     alert("Thanks "+name+" for planting "+plant+" in bed "+bedNum);
 }
