dde44c33e235a7c21ed99908a4836d91e95a6479
joeroybal <joewroybal@gmail.com>
2021-04-07 18:58:27 -0500

    debug

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def Outside():
 def contents(id_):
     try:
         bed = Plants.query.filter_by(bed=id_).order_by(Plants._id.desc()).first()
-        return render_template('bed-contents.html',bedNum=bed.bed, name=bed.owner, plant=bed.plant_species, date_planted=bed.date_planted, date_finished=bed.date_finished, last_watered=bed.last_watered)
+        return render_template('bed-contents.html',bedNum=bed.bed, name=bed.owner, plant=bed.plant_species, date_planted=bed.date_planted, date_finished=bed.date_finish, last_watered=bed.last_watered)
     except Exception as e:
         return(str(e))
 
