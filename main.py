5b96ff8758e570914fab820e6aca454a4d678cb4
JoeRoybal <joewroybal@gmail.com>
2021-04-16 11:18:35 -0500

    testing hasher

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os 
-from flask import Flask, config,render_template,request, flash, redirect, url_for, jsonify
+import os
+from flask import Flask, config, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
 from werkzeug.wrappers import UserAgentMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -9,14 +9,15 @@ from hashlib import sha256
 
 from sqlalchemy.ext.hybrid import hybrid_property
 import psycopg2
-from wtforms import StringField, Form,validators, TextField, TextAreaField, SubmitField, PasswordField
+from wtforms import StringField, Form, validators, TextField, TextAreaField, SubmitField, PasswordField
 
 # Creates Flask app
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-app.secret_key= os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+app.secret_key = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL').replace("://", "ql://", 1)
 DATABASE_URL = os.environ['DATABASE_URL']
 
 # Creates database using SQLAlchemy in the app
@@ -24,18 +25,25 @@ db = SQLAlchemy(app)
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
+
 class ReusableForm(Form):
     owner = TextField('Owner: ', validators=[validators.required()])
-    plant_species = TextField('Plant Species: ', validators=[validators.required()])
-    date_planted = TextField('Date Planted: ', validators=[validators.required()])
-    date_finish = TextField('Date Finished: ', validators=[validators.required()])
-    last_watered = TextField('Last Watered: ', validators=[validators.required()])
+    plant_species = TextField('Plant Species: ', validators=[
+                              validators.required()])
+    date_planted = TextField('Date Planted: ', validators=[
+                             validators.required()])
+    date_finish = TextField('Date Finished: ', validators=[
+                            validators.required()])
+    last_watered = TextField('Last Watered: ', validators=[
+                             validators.required()])
     owner = TextField('Date PLanted: ', validators=[validators.required()])
     temp = TextField('Temperature: ', validators=[validators.required()])
     humidity = TextField('Humidity: ', validators=[validators.required()])
-    soil_moisture = TextField('Soil Moisture: ', validators=[validators.required()])
+    soil_moisture = TextField('Soil Moisture: ', validators=[
+                              validators.required()])
     bed = TextField('Bed Number: ', validators=[validators.required()])
-    
+
+
 class Plants(db.Model):
     __tablename__ = "greenhouse"
     _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -48,13 +56,12 @@ class Plants(db.Model):
     humidity = db.Column(db.String(100), nullable=False)
     soil_moisture = db.Column(db.String(100), nullable=False)
     bed = db.Column(db.String(100), nullable=False, primary_key=True)
-    
-    
+
     @hybrid_property
     def password(self):
         self._password
-    
-    def __init__(self, plant_species, owner,date_planted, date_finish, last_watered, temp, humidity, soil_moisture, bed, _password):
+
+    def __init__(self, plant_species, owner, date_planted, date_finish, last_watered, temp, humidity, soil_moisture, bed, _password):
         self.plant_species = plant_species
         self.owner = owner
         self.date_planted = date_planted
@@ -64,7 +71,7 @@ class Plants(db.Model):
         self.humidity = humidity
         self.soil_moisture = soil_moisture
         self.bed = bed
-        
+
     def serialize(self):
         return{
             'bed': self.bed,
@@ -72,9 +79,10 @@ class Plants(db.Model):
             'plant': self.plant_species,
             'date_planted': self.date_planted,
             'date_finished': self.date_finish,
-            'last_watered' : self.last_watered
+            'last_watered': self.last_watered
         }
-        
+
+
 class User(db.Model):
     __tablename__ = "userCred"
     id = db.Column(db.Integer, primary_key=True)
@@ -88,55 +96,66 @@ def Home():
     return render_template('index.html', title='Home')
 
 # Front route
+
+
 @app.route('/front')
 def Front():
     return render_template('front.html', title='Front')
 
 # Back route
+
+
 @app.route('/back')
 def Back():
     return render_template('back.html')
 
 # Outside route
+
+
 @app.route('/outside')
 def Outside():
-    return render_template('outside.html')  
+    return render_template('outside.html')
 
-@app.route('/auth', methods = ["GET","POST"])
+
+@app.route('/auth', methods=["GET", "POST"])
 def auth():
     if request.method == 'POST':
         name = request.form['name']
         password = request.form['password']
-        #hash password
+        # hash password
         hashword = sha256(password.encode('utf-8')).hexdigest()
-        
+
         user = User.query.filter_by(name=name).first()
-        
-        if not user or not check_password_hash(user.password, hashword):
+
+        if user.password == hashword:
             return render_template('UserAuth.html', user=user, hashword=password)
-        
+
         return redirect('/form')
-    
+
     return render_template('UserAuth.html')
-    
+
+
 @app.route('/bed-contents/<id_>')
 def contents(id_):
     try:
-        bed = Plants.query.filter_by(bed=id_).order_by(Plants._id.desc()).first()
+        bed = Plants.query.filter_by(bed=id_).order_by(
+            Plants._id.desc()).first()
         if bed != None:
-            return render_template('bed-contents.html',bedNum=bed.bed, name=bed.owner, plant=bed.plant_species, date_planted=bed.date_planted, date_finished=bed.date_finish, last_watered=bed.last_watered)
+            return render_template('bed-contents.html', bedNum=bed.bed, name=bed.owner, plant=bed.plant_species, date_planted=bed.date_planted, date_finished=bed.date_finish, last_watered=bed.last_watered)
         else:
             return render_template('bedErr.html', bedNum=id_)
     except Exception as e:
         return(str(e))
 
 # Page for forms
-@app.route('/form', methods = ["GET", "POST"])
+
+
+@app.route('/form', methods=["GET", "POST"])
 @login_required
 def form():
     form = ReusableForm(request.form)
-    if request.method == 'POST': # submit
-        data= request.form # data from the form
+    if request.method == 'POST':  # submit
+        data = request.form  # data from the form
         date_planted = request.form['date_planted']
         plant_species = request.form['plant_species']
         owner = request.form['owner']
@@ -146,10 +165,11 @@ def form():
         humidity = request.form['humidity']
         soil_moisture = request.form['soil_moisture']
         bed = request.form['bed']
-        new_data = Plants(plant_species, owner,date_planted, date_finish, last_watered, temp, humidity, soil_moisture, bed)
+        new_data = Plants(plant_species, owner, date_planted, date_finish,
+                          last_watered, temp, humidity, soil_moisture, bed)
         db.session.add(new_data)
         db.session.commit()
-        
+
         user_data = Plants.query.all()
         return redirect('/')
-    return render_template('testing.html',form = form, title='Planting Form') 
+    return render_template('testing.html', form=form, title='Planting Form')
